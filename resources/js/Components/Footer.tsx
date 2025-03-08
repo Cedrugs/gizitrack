f5ed0c0303eba87029c2f855e2bcfc42b44cd572
Samuel Cedric <ceds.sam@gmail.com>
2025-03-08 14:30:37 +0700

    First commit

---
+++ resources/js/Components/Footer.tsx
@@ -0,0 +1,33 @@
+import InstagramIcon from "@/Assets/icons/instagram.png"
+import FacebookIcon from "@/Assets/icons/facebook.png"
+import LinkedinIcon from "@/Assets/icons/linkedin.png"
+import LogoMedium from "@/Assets/logos/logo-md.png"
+
+export default function Footer() {
+    return (
+        <footer className="flex max-md:h-auto lg:h-[258px] bg-[#3F5B71] items-center py-12">
+            <div className="flex lg:flex-row flex-wrap items-start justify-around w-full gap-5 max-md:text-center">
+                <div className="flex flex-col items-center gap-4">
+                    <p className="font-semibold text-2xl text-[#EFE9D5]">GiziTrack</p>
+                    <img src={LogoMedium} alt="Logo Icon" className="w-24"/>
+                </div>
+                <div className="flex flex-col lg:items-left gap-4 max-w-96">
+                    <p className="font-semibold text-xl text-[#EFE9D5]">Contact Us</p>
+                    <div className="flex flex-col text-sm gap-2 text-[#EFE9D5]">
+                        <p>Jl. Jalur Sutera Bar. No.Kav. 21, RT.001/RW.004, Panunggangan, Kec. Pinang Kota Tangerang Banten</p>
+                        <p>+62 123 4567 8900</p>
+                        <p>ureeka.group6@binus.ac.id</p>
+                    </div>
+                </div>
+                <div className="flex flex-col  lg:items-left gap-4 max-w-96">
+                    <p className="font-semibold text-xl text-[#EFE9D5]">Follow Us</p>
+                    <div className="flex flex-row text-sm gap-8 text-[#EFE9D5]">
+                        <img src={LinkedinIcon} alt="Linkedin Icon" />
+                        <img src={InstagramIcon} alt="Instagram Icon" />
+                        <img src={FacebookIcon} alt="Facebook Icon" />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
